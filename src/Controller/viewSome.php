1e1038e8bd65e544ba34431df15432e49dc35754
shanemc92 <shanemc92@pm.me>
2019-04-15 14:14:05 +0100

    Manager Page Completed

--- src/Controller/viewSome.php
+++ src/Controller/viewSome.php
@@ -27,8 +27,10 @@ class viewSome extends AbstractController
        
         $output = ''; // html the user will see
 
-        $output = '
-        <table data-role="table" id="ordersTable"  class="ui-body-d ui-shadow table-stripe ui-responsive ui-table">
+        $cost = 0;
+        $count = '';
+
+        $output = '<table data-role="table" id="ordersTable"  class="ui-body-d ui-shadow table-stripe ui-responsive ui-table">
             <thead>
                 <tr class="ui-bar-d">
                     <th>Order</th>
@@ -42,14 +44,15 @@ class viewSome extends AbstractController
              </thead>
         <tbody>'; // html the user will see
 
+        
 
         foreach($orders as $pro){
             $datecreated = $pro->getCreated();
-            $date = $datecreated->format('m/d/Y');
+            $date = $datecreated->format('Y-m-d');
             //$date1 = strtotime($date1);
             //$date2 = strtotime($date2);
             if($date >= $date1 && $date <= $date2){
-                $output .= '<tr>'; // one row
+                    $output .= '<tr>'; // one row
                     $output .= '<td>' . $pro->getId() . '</td>';
                     $output .= '<td>' . $pro->getPlacedby() . '</td>';
                     $detail = explode('+=====+', $pro->getDetails()); // break it apart based on +=.
@@ -57,18 +60,17 @@ class viewSome extends AbstractController
                     $output .= '<td>' . $detail[1] . '</td>'; // Address
                     $output .= '<td>' . $detail[2] . '</td>'; // Total
                     $output .= '<td>' . $pro->getStatus() . '</td>';
-                    $output .= '<td><div class="ui-field-contain" orderid="'. $pro->getId() .'">
-                                        <select name="orderStatus" id="orderStatus">
-                                            <option value="1">Getting Ready</option>
-                                            <option value="2">In Progress</option>
-                                            <option value="3">Completed</option>
-                                        </select>
-                                    </div></td>';
-                $output .= '</tr>';    
+                    $output .= '<td><button class="cancelOrder ui-btn" orderid="'. $pro->getId() .'">Cancel Order</button></td>';
+                $output .= '</tr>';
+            $indcost = explode('€', $detail[2]);
+            $cost += (int)str_replace(' ', '', $indcost[1]);
+            $count ++; 
             }
         }
         $output .= '</tbody></table>';
+        $totals = '<p><b>Total Orders</b>: '.$count.'<br><b>Total Revenue:</b> €'.$cost.'<br>';
 
+        $output = $totals . $output;
         return new Response(
             $output
         );
